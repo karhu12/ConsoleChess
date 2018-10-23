c65dd3a57dae631e3b0d9872ec4162ed770cb7cf
Riku Kaipainen <shylessfly@gmail.com>
2018-10-23 23:57:00 +0300

    oldPixelGameEngine succesfully included into the project. Starting to work on Ui.

--- include/Ui.hpp
+++ include/Ui.hpp
@@ -1,5 +1,27 @@
 #pragma once
+#include <olcPixelGameEngine.h>
 
-class Ui {
+class Ui : public olc::PixelGameEngine
+	{
+	public:
+		Ui()
+		{
+			sAppName = "Example";
+		}
 
-};
+	public:
+		bool OnUserCreate() override
+		{
+			// Called once at the start, so create things here
+			return true;
+		}
+
+		bool OnUserUpdate(float fElapsedTime) override
+		{
+			// called once per frame, draws random coloured pixels
+			for (int x = 0; x < ScreenWidth(); x++)
+				for (int y = 0; y < ScreenHeight(); y++)
+					Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand()% 255));
+			return true;
+		}
+	};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+#define OLC_PGE_APPLICATION
 #include <ChessGame.hpp>
+#include <Ui.hpp>
+
+/*
+Olc Pixel Game Engine used in this project.
+Created by OneLoneCoder
+*/
 
 int main() {
-    ChessGame game;
-    game.start();
+    Ui ui;
+    if (ui.Construct(256, 240, 4, 4))
+        ui.Start();
     return 0;
 }
